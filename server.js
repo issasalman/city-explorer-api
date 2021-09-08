e8b31112f83d1ae99d931af56e1b290ec06c3632
issasalman <issasalmanhu1996@gmail.com>
2021-09-08 06:25:33 +0300

    lab08

--- server.js
+++ server.js
@@ -1,61 +1,119 @@
 "use strict";
-
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const axios = require("axios");
 app.use(cors());
-const weather = require("./data/weather.json");
+
+// const weather = require("./data/weather.json");
 
 class Forecast {
   constructor(description, date) {
     this.description = description;
     this.date = date;
-    Forecast.all.push(this);
+    // Forecast.all.push(this);
   }
 }
-Forecast.all = [];
+// Forecast.all = [];
+
+class Moovies {
+  constructor(title, overview, vote, count) {
+    this.title = title;
+    this.overview = overview;
+    this.vote = vote;
+    this.count = count;
+  }
+}
 
 app.get("/", function (request, response) {
-  response.send("Hello 301👋 In this API u can know the weather of any location You want Like :Amman And Seattle  ");
+  response.send(
+    "Hello 301👋 In this API u can know the weather of any location You want Like :Amman And Seattle Also we can provide for you a movie related to location name  "
+  );
 });
 
-// app.get("/weatherInfo", function (request, response) {
-//   response.json(Forecast.all[0]);
-// });
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
-app.get("/weather", (request, response) => {
+app.get("/weather", async (request, response) => {
+  // const lon = request.query.lon;
+  const city_name = request.query.city;
+  // const lat = request.query.lat;
+  // console.log(lat);
+  // console.log(city_name);
+  const weatherBitUrl = "https://api.weatherbit.io/v2.0/forecast/daily";
+  const weatherBitResponse = await axios.get(
+    `${weatherBitUrl}?city=${city_name}&key=${WEATHER_API_KEY}`
+  );
 
-  
-  const lon = request.query.lon;
-  const city_name = request.query.city_name;
-  const lat = request.query.lat;
-  console.log(lat);
-  console.log(city_name);
-  console.log(lon);
+  // console.log("hello",WEATHER_API_KEY);
+  // console.log(movieResponse);
 
   if (city_name) {
-    const returnArray = weather.find((item) => {
-      return item.city_name.toLowerCase() === city_name;
-    });
-    // console.log(returnArray);
-    let arr1=returnArray.data.map((data1) => {
-      // console.log(data1.weather.description);
+    // const returnArray = movieResponse.data.data.find((item) => {
+    //   console.log(item);
+    //   return item.city_name.toLowerCase() === city_name;
+    // });
+    // // console.log(returnArray);
+    let arr1 = weatherBitResponse.data.data.map((data1) => {
+      // console.log(data1);
       return new Forecast(
         ` Low of ${data1.low_temp}, high of ${data1.high_temp} with ${data1.weather.description} `,
         ` ${data1.datetime}`
       );
     });
 
-    // console.log(arr1);
-
-    // console.log(Forecast);
+    let arr2 = arr1[0];
     if (arr1.length) {
-      response.json(arr1);
+      response.json(arr2);
     } else {
       response.send("error: Something went wrong.");
     }
   } else {
-    response.json(weather);
+    response.json("error: Something went wrong.");
+  }
+});
+
+const MOVIES_API_KEY = process.env.MOVIES_API_KEY;
+
+app.get("/movies", async (request, response) => {
+  // const lon = request.query.lon;
+  const city_name = request.query.query;
+
+  // const lat = request.query.lat;
+  // console.log(lat);
+  // console.log(city_name);
+  const movie = "https://api.themoviedb.org/3/search/movie";
+  const movieResponse = await axios.get(
+    `${movie}?query=${city_name}&api_key=${MOVIES_API_KEY}`
+  );
+
+  // console.log("hello",WEATHER_API_KEY);
+
+  // response.json("error: Something went wrong.");
+  if (city_name) {
+    // const returnArray = movieResponse.data.data.find((item) => {
+    //   console.log(item);
+    //   return item.city_name.toLowerCase() === city_name;
+    // });
+    // // console.log(returnArray);
+    let arr1 = movieResponse.data.results.map((data1) => {
+      console.log(data1);
+      return new Moovies(
+        `Title: ${data1.title}`,
+        `Overview: ${data1.overview}`,
+        `Average votes: ${data1.vote_average}`,
+        ` Total Votes: ${data1.vote_count}`
+      );
+    });
+
+    let arr2 = arr1[0];
+    if (arr1.length) {
+      response.json(arr2);
+    } else {
+      response.send("error: Something went wrong.");
+    }
+  } else {
+    response.json("error: Something went wrong.");
   }
 });
 
