034cb99251073fda6e44c75658c438918cf78f15
issasalman <issasalmanhu1996@gmail.com>
2021-09-11 22:41:00 +0300

    lab10

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,35 +1,55 @@
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-const Forecast = require('../models/weather.model');
+const Forecast = require("../models/weather.model");
+const Cache = require("../helper/cache.helper");
+let cacheObject = new Cache();
 
 const getWeather = async (request, response) => {
-  // const lon = request.query.lon;
   const city_name = request.query.city;
-  // const lat = request.query.lat;
-  // console.log(lat);
-  // console.log(city_name);
-  const weatherBitUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
-  const weatherBitResponse = await axios.get(
-    `${weatherBitUrl}?city=${city_name}&key=${WEATHER_API_KEY}`
+
+
+  const shutTime = 60000;
+  const oneMinute = (Date.now() - cacheObject.timeStamp) > shutTime;
+  if (oneMinute) {
+   
+    cacheObject = new Cache();
+  }
+
+
+  const findData = cacheObject.forCast.find(
+    (location) => location.city_name === city_name
   );
-
-  if (city_name) {
-    let arr1 = weatherBitResponse.data.data.map((data1) => {
-      return new Forecast(
-        ` Low of ${data1.low_temp}, high of ${data1.high_temp} with ${data1.weather.description} `,
-        ` ${data1.datetime}`
-      );
-    });
-
-    if (arr1.length) {
-      response.json(arr1);
-      console.log(arr1);
-    } else {
-      response.send('error: Something went wrong.');
-    }
+  if (findData) {
+    response.json(findData.data);
   } else {
-    response.json('error: Something went wrong.');
+    const weatherBitUrl = "https://api.weatherbit.io/v2.0/forecast/daily";
+    const weatherBitResponse = await axios.get(
+      `${weatherBitUrl}?city=${city_name}&key=${WEATHER_API_KEY}`
+    );
+
+    if (city_name) {
+      let data = weatherBitResponse.data.data.map((data1) => {
+        return new Forecast(
+          ` Low of ${data1.low_temp}, high of ${data1.high_temp} with ${data1.weather.description} `,
+          ` ${data1.datetime}`
+        );
+      });
+
+      cacheObject.forCast.push({
+        city_name: city_name,
+        data: data,
+      });
+      console.log(cacheObject);
+      if (data.length) {
+        response.json(data);
+        console.log(data);
+      } else {
+        response.send("error: Something went wrong.");
+      }
+    } else {
+      response.json("error: Something went wrong.");
+    }
   }
 };
 
